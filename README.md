aa7d29a87b2e213e66082598445603d6fed3ab1c
paulJdN <paul.jouslin@gmail.com>
2022-03-20 19:58:17 +0100

    updatecontacto

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # practica5
 Practica 5 : Empleando un formularion de contacto con Stringboot
+
+Springboot con un metodo post en el formulario contacto.
+
+Para empiezar el springboot app : mvn spring-boot:run
--- demo/src/main/java/com/example/demo/controller/ContactController.java
+++ demo/src/main/java/com/example/demo/controller/ContactController.java
@@ -2,16 +2,31 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ContactController {
 
-    @PostMapping("/save-email")
+    @PostMapping("save-email")
     @ResponseBody
-    public String sendEmail(){
+    public String sendEmail(@RequestParam (name="name") String name, 
+                            @RequestParam (name="email") String email, 
+                            @RequestParam (name="phone") String phone,
+                            @RequestParam (name="message") String message){
 
-        return " svaed";
+        return "<html>"+
+                    "<head> <title= 'Acciona - Respuesta contacto'><head>"+
+                    "<body align='center'>"+
+                        "<p> Muchas gracias por el mensaje, te contestamos dentro de una semana</p>"+
+                        "<p> Aqui un recapitulo</p>"+
+                        "<p> Name : "+ name+"</p>"+
+                        "<p> Email : "+ email+"</p>"+
+                        "<p> Telefono : "+ phone+"</p>"+
+                        "<p> Mensaje : "+ message+"</p>"+
+                        "<button> <a href='index.html'>Volver al inicio </a></button>"+
+                    "</body>"+
+                "</html>";
     }
     
 }
